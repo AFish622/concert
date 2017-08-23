6ea0585237d8dbbe6e8225ca772602050ee70aa5
Austin Fisher <AFish622@gmail.com>
2017-08-22 19:22:00 -0700

    updated initial test

--- server.js
+++ server.js
@@ -5,3 +5,5 @@ const bodyParser = require('body-parser');
 app.use(express.static('public'));
 
 app.listen(process.env.PORT || 8080);
+
+exports.app = app;
--- test/test-integration.js
+++ test/test-integration.js
@@ -1,16 +1,20 @@
 const mocha = require('mocha');
 const chai = require('chai');
 const chaiHttp = require('chai-http');
-const mongoose = require('mongoose');
-const express = require('express');
+const server = require('../server.js')
 
-const app = express();
+const app = server.app;
 const should = chai.should();
 chai.use(chaiHttp)
 
-describe('App', function() {
-	it('Should work', function() {
-		return chai.request(app)
-		res.should.have.status(200);
-	})
-})
+describe('index page', function() {
+  it('exists', function(done) {
+    chai.request(app)
+      .get('/')
+      .end(function(err, res) {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+    });
+  });
+});
